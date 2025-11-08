f758483569623563d6d5c3b5734a9b5008034c95
Alan Garcia <garciaalan0421@gmail.com>
2025-11-08 14:00:57 -0600

    Added an update call for products

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -55,6 +56,22 @@ app.get("/api/products", async (req, res) => {
         res.status(200).json({ success: true, data: products });
     } catch(error) {
         console.log("Error in fetching products: ", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const updates = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Product not found" });
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, updates)
+        res.status(200).json({ success: true, data: updatedProduct });
+    }catch (error) {
         res.status(500).json({ success: false, message: "Server Error" });
     }
 });
