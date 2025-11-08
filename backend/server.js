b5ac0f356c7d79c99ffc685d67adce6bf91fbae0
Alan Garcia <garciaalan0421@gmail.com>
2025-11-08 11:43:44 -0600

    Added post call for products

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,34 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/products", (req, res) => {
-    res.send("Server is ready");
+app.post("/products", async (req, res) => {
+    const product = req.body; //user will send product data in request body
+
+    // Validate product data
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" });
+    }
+
+    //Save product to database
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.log("Error in creating product: ", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
 });
 
-console.log(process.env.MONGO_URI);
-
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
