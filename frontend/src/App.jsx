b4c1c6cf74f5af1533f6015c0d0967ca13eef001
Alan Garcia <garciaalan0421@gmail.com>
2026-01-03 15:43:46 -0600

    Added comments for clarity and understanding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import CreatePage from './pages/CreatePage.jsx';
 import Navbar from './components/Navbar.jsx';
+import { useColorModeValue } from '@chakra-ui/react';
 
 function App() {
   return (
-    <Box minH={"100vh"}>
+    /*
+      Box component to hold the entire app
+      minH: minimum height of 100vh to cover full viewport height
+      bg: background color based on color mode
+        - gray.100 for light mode
+        - gray.900 for dark mode
+    */
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       {/* Navbar */}
       <Navbar />
         <Routes>
