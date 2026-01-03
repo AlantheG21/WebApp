00375c4391c2d110158b08de233dc423e194e070
AlanGarcia <garciaalan0421@gmail.com>
2026-01-02 22:11:35 -0600

    Imported functions from products controller

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,72 +1,18 @@
 import express from 'express';
-import Product from '../models/product.model.js';
-import mongoose from 'mongoose';
+import { createProduct, deleteProduct, getProducts, updateProduct } from '../controllers/product.controller.js';
 
 const router = express.Router();
 
 // Endpoint to create a new product
-router.post("/", async (req, res) => {
-    const product = req.body; //user will send product data in request body
-
-    // Validate product data
-    if(!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: "Please provide all fields" });
-    }
-
-    //Save product to database
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-        console.log("Error in creating product: ", error.message);
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-});
+router.post("/", createProduct);
 
 // Endpoint to delete a product by ID
-router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    console.log("Deleting product with id: ", id);
-
-    try {
-        await Product.findByIdAndDelete(id);
-        console.log("Product deleted successfully");
-        res.status(200).json({ success: true, message: "Product deleted successfully" });
-    }catch (error) {
-        console.log("Error in deleting product: ", error.message);
-        res.status(404).json({ success: false, message: "Product not found" });
-    }
-
-});
+router.delete("/:id", deleteProduct);
 
 // Endpoint to get all products
-router.get("/", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({ success: true, data: products });
-    } catch(error) {
-        console.log("Error in fetching products: ", error.message);
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-});
+router.get("/", getProducts);
 
 // Endpoint to update a product by ID
-router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const updates = req.body;
-
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ success: false, message: "Product not found" });
-    }
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, updates)
-        res.status(200).json({ success: true, data: updatedProduct });
-    }catch (error) {
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-});
+router.put("/:id", updateProduct);
 
 export default router;
