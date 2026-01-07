4d1abe67b4b0c2ccab32e7996f3f79831d5a6241
AlanGarcia <garciaalan0421@gmail.com>
2026-01-06 21:47:36 -0600

    Served static assets to deploy project

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+
 import { connectDB } from './config/db.js';
 
 import productRoutes from './routes/product.route.js';
@@ -7,12 +9,21 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
-
-app.use(express.json()); // Middleware to parse JSON bodies
-
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // Middleware to parse JSON bodies
+const __dirname = path.resolve(); // Get current directory path
+
 app.use("/api/products", productRoutes)
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist'))); // Serve static files from the React app
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')); // Serve the main HTML file for any unknown routes
+    });
+}
 
 app.listen(PORT, () => {
     connectDB();
